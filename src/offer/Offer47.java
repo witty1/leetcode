cc46986963ee786269f99939a89620bd04c5bfb7
witty1 <3168606081@qq.com>
2021-01-13 17:32:01 +0800

    格式化

--- src/offer/Offer47.java
+++ src/offer/Offer47.java
@@ -5,15 +5,15 @@ package offer;
  * @author: wit
  * @Date: 2021/1/13
  * @Time: 16:39
- * @Description: No Description
+ * @Description: https://leetcode-cn.com/problems/li-wu-de-zui-da-jie-zhi-lcof/
  */
 public class Offer47 {
     public int maxValue(int[][] grid) {
         int row = grid.length, line = grid[0].length;
-        int[][] count = new int[row+1][line+1];
+        int[][] count = new int[row + 1][line + 1];
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= line; j++) {
-                count[i][j] = Math.max(count[i-1][j],count[i][j-1])+grid[i-1][j-1];
+                count[i][j] = Math.max(count[i - 1][j], count[i][j - 1]) + grid[i - 1][j - 1];
             }
         }
         return count[row][line];
